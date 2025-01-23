15e7074372771c9ac801504f32e43f4eb1307bb2
cpedrorafael <pedro.cutino23@gmail.com>
2025-01-23 17:39:10 -0500

    music + videos

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,22 +1,35 @@
 package com.guayaba.youtubeMusicDownload
 
 import com.guayaba.youtubeMusicDownload.YoutubeDownloader.Companion.downloadSongsFromFile
+import com.guayaba.youtubeMusicDownload.YoutubeDownloader.Companion.downloadVideosFromFile
 import kotlinx.coroutines.async
 
 
 suspend fun main() {
     val logger = Logger("application.log")
 
-    println("please insert the working directory (must be contain a list.txt file with the song names): ")
+    println("""
+        please choos e mode (or enter to use default mode):
+        1- Music (default)
+        2- Videos
+    """.trimIndent())
+    val mode: Int = readln().toInt()
+
+    println("please insert the working directory (must be contain a list.txt file with the song names, or video URLs): ")
 
     val filePath = readln()
+
     try {
         val deferred = AsyncManager.scope.async {
-            downloadSongsFromFile(filePath, logger)
+            when(mode){
+                1 -> downloadSongsFromFile(filePath, logger)
+                2 -> downloadVideosFromFile(filePath, logger)
+                else -> throw IllegalStateException("Invalid mode")
+            }
         }
         deferred.await()
-    } catch (_: Exception) {
-
+    } catch (e: Exception) {
+        logger.logException(e)
     } finally {
         logger.close()
     }
--- src/main/kotlin/YoutubeDownloader.kt
+++ src/main/kotlin/YoutubeDownloader.kt
@@ -25,7 +25,7 @@ class YoutubeDownloader {
             throw Exception("Yt-dlp executable not found")
         }
 
-        private fun downloadVideo(videoUrl: String, title: String, outputPath: String, logger: Logger? = null) {
+        private fun downloadVideoAsMP3(videoUrl: String, title: String, outputPath: String, logger: Logger? = null) {
             println("\nAttempting to download $title")
             try {
                 val ytDlpPath = getExecutablePath()
@@ -34,6 +34,33 @@ class YoutubeDownloader {
                     "-f", "ba", // Best audio
                     "-x", // Extract audio only
                     "--audio-format", "mp3", // Convert to mp3
+                    videoUrl,
+                    "-o", "$outputPath%(title)s.%(ext)s"
+                )
+                val runtime = Runtime.getRuntime()
+                val process = runtime.exec(ytDlpCmd)
+
+                process.inputStream.bufferedReader().use {
+                    it.lines().forEach { line -> println(line) }
+                }
+                process.waitFor()
+                if (process.exitValue() != 0) {
+                    throw RuntimeException("yt-dlp command failed with exit code ${process.exitValue()}")
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                logger?.logException(e)
+            }
+        }
+
+        private fun downloadVideo(videoUrl: String, outputPath: String, logger: Logger? = null) {
+            println("\nAttempting to download $videoUrl")
+            try {
+                val ytDlpPath = getExecutablePath()
+                val ytDlpCmd = arrayOf(
+                    ytDlpPath,
+                    "-f", "bestvideo+bestaudio/best", // Download the best video and audio streams, combine them if necessary
+                    "--merge-output-format", "mp4", // Ensure the output is merged into a single MP4 file if separate streams are downloaded
                     videoUrl,
                     "-o", "$outputPath%(title)s.%(ext)s"
                 )
@@ -79,8 +106,34 @@ class YoutubeDownloader {
                         if (songUrl.isNullOrEmpty()) {
                             logger.logException(java.lang.Exception("URL for song $song not found"))
                         } else {
-                            downloadVideo(songUrl, song, "$outputPath/", logger)
+                            downloadVideoAsMP3(songUrl, song, "$outputPath/", logger)
                         }
+                    }
+                }
+            }
+
+            jobs.joinAll()
+        }
+
+        suspend fun downloadVideosFromFile(workingDir: String, logger: Logger) {
+
+            var outputPath = workingDir
+            if (workingDir.last() == '/') {
+                outputPath = outputPath.substring(workingDir.indices)
+            }
+
+            val listFile = File("$outputPath/list.txt")
+
+            if (!listFile.exists()) {
+                throw IllegalStateException("list.txt file cannot be found in directory")
+            }
+
+            val URLs = readLinesFromFile(listFile.absolutePath)
+
+            val jobs = URLs.map { url ->
+                AsyncManager.scope.launch {
+                    AsyncManager.semaphore.withPermit {
+                        downloadVideo(url,"$outputPath/", logger)
                     }
                 }
             }
